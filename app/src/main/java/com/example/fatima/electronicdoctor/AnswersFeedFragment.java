05d33259b59997e80fe8bccf07d65d0a07742600
Fatima <capt.fatimaAli@gmail.com>
2019-12-19 01:35:16 +0300

    .

---
+++ app/src/main/java/com/example/fatima/electronicdoctor/AnswersFeedFragment.java
@@ -0,0 +1,103 @@
+package com.example.fatima.electronicdoctor;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswersFeedFragment extends Fragment {
+    public static Fragment newInstance() {
+        return new AnswersFeedFragment();
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.recycler_view_answers_fragment, container,false);
+
+        List<String> list=new ArrayList<>();
+        list.add("FINGERS AND HOW THEY COULD KILL YOU AND EVERYONE YOU KNOW ");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+ String[] str=new String[5];
+ str[0]="Ss";
+ str[1]="frf";
+ str[2]="vfvdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvx";
+ str[3]="dxvfx vfdxvfdxvxdxvfx vfdxvfdxvx";
+ str[4]="dxvfxnhnyn";
+
+        String[] str2=new String[5];
+        str2[0]="22Ss";
+        str2[1]="22frf";
+        str2[2]="evevvfvdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfxvfvdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfxvfvdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfxvfvdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvxdxvfx vfdxvfdxvx";
+        str2[3]="dxvfx vfdxvfdxvxdxvfx vfdxvfdxvx";
+        str2[4]="dxvfxnhnyn";
+
+//        List<List<String>> listOfLists = new ArrayList<List<String>>();
+//        listOfLists.add(new ArrayList<String>());
+
+        RecyclerView recyclerView = view.findViewById(R.id.answers_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new RecyclerViewAdapter(str,str2));
+        return view;
+
+    }
+    private class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        private FrameLayout mFrameLayout;
+        private TextView docName;
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public RecyclerViewHolder(LayoutInflater inflater, ViewGroup container){
+            super(inflater.inflate(R.layout.answered_questions,container,false));
+            mFrameLayout =itemView.findViewById(R.id.answered_question_frame);
+
+        }
+    }
+
+    private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder>{
+
+        private String[] mList,s;
+        public RecyclerViewAdapter(String[] list, String[] s){
+            this.mList=list;
+            this.s=s;
+
+        }
+
+
+
+
+        @NonNull
+        @Override
+        public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            return new RecyclerViewHolder(inflater, viewGroup);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerViewHolder recyclerViewHolder, int i) {
+
+//            recyclerViewHolder.postTitle.setText(mList[i]);
+//            recyclerViewHolder.postDescription.setText(s[i]);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return s.length;
+        }
+    }
+
+}
--- app/src/main/java/com/example/fatima/electronicdoctor/MainActivity.java
+++ app/src/main/java/com/example/fatima/electronicdoctor/MainActivity.java
@@ -1,23 +1,64 @@
 package com.example.fatima.electronicdoctor;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    fragment = new RecyclerFragment();
+                    break;
+
+                case R.id.navigation_answered_questions:
+
+                    fragment = new AnswersFeedFragment();
+                    break;
+//                    Intent myIntent = new Intent(MainActivity.this, MainActivity.class);
+//                    MainActivity.this.startActivity(myIntent);
+                case R.id.navigation_ask_question:
+                    fragment = new WritePostFragment();
+                    break;
+
+//                    return true;
+            }
+            return loadFragment(fragment);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new ProfileFragment());
 
-
-        //System.out.println();
-
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+       navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
